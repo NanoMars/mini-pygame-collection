a42d02b86adb676e375a4ccc063ed5758d23ccbc
NanoMars <174164929+NanoMars@users.noreply.github.com>
2024-08-14 15:10:03 +1000

    ok I think I finished the tutorial

--- Games/Maze.py
+++ Games/Maze.py
@@ -1,7 +1,7 @@
 import pygame
 from random import choice
 
-RES = WIDTH, HEIGHT = 800, 600
+RES = WIDTH, HEIGHT = 1200, 900
 TILE = 50
 cols, rows = WIDTH // TILE, HEIGHT // TILE
 
@@ -12,40 +12,42 @@ clock = pygame.time.Clock()
 class Cell:
     def __init__(self, x, y):
         self.x, self.y = x, y
-        self.walls = {
-            'top': True,
-            'right': True,
-            'bottom': True,
-            'left': True
-        }
-    def draw(self):
-        x, y = self.x * TILE, self.y * TILE
-        if self.visited:
-            pygame.draw.line(sc, pygame.Color('1e1e1e'), (x, y, TILE, TILE))
-            
-        if self.walls['top']:
-            pygame.draw.line(sc, pygame.Color('1e4f5b'), (x, y), (x + TILE, y), 3) 
-        if self.walls['right']:
-            pygame.draw.line(sc, pygame.Color('1e4f5b'), (x + TILE, y), (x + TILE, y + TILE), 3)
-        if self.walls['bottom']:
-            pygame.draw.line(sc, pygame.Color('1e4f5b'), (x + TILE, y + TILE), (x, y + TILE), 3)
-        if self.walls['left']:
-            pygame.draw.line(sc, pygame.Color('1e4f5b'), (x, y + TILE), (x, y), 3)
-            
+        self.walls = {'top': True, 'right': True, 'bottom': True, 'left': True}
+        self.visited = False
+    
     def draw_current_cell(self):
         x, y = self.x * TILE, self.y * TILE
-        pygame.draw.rect(sc, pygame.Color('f70067'), (x + 2, y + 2, TILE - 2, TILE - 2))
+        pygame.draw.rect(sc, pygame.Color('#f70067'), (x + 2, y + 2, TILE - 2, TILE - 2))
+    
+    def draw(self):
+        x, y = self.x * TILE, self.y * TILE
+        
+        if self.visited:
+            pygame.draw.rect(sc, pygame.Color('#1e1e1e'), (x, y, TILE, TILE))
+        
+        if self.walls['top']:
+            pygame.draw.line(sc, pygame.Color('#1e4f5b'), (x, y), (x + TILE, y), 3)
+        if self.walls['right']:
+            pygame.draw.line(sc, pygame.Color('#1e4f5b'), (x + TILE, y), (x + TILE, y + TILE), 3)
+        if self.walls['bottom']:
+            pygame.draw.line(sc, pygame.Color('#1e4f5b'), (x + TILE, y + TILE), (x, y + TILE), 3)
+        if self.walls['left']:
+            pygame.draw.line(sc, pygame.Color('#1e4f5b'), (x, y + TILE), (x, y), 3)
+            
     def check_cell(self, x, y):
         find_index = lambda x, y: x + y * cols
-        if x < 0 or x > cols - 1 or y < 0 or y > rows - 1:
-            return False
+        if x < 0 or x >= cols or y < 0 or y >= rows:
+            return None 
         return grid_cells[find_index(x, y)]
+    
     def check_neighbors(self):
         neighbors = []
+        
         top = self.check_cell(self.x, self.y - 1)
         right = self.check_cell(self.x + 1, self.y)
         bottom = self.check_cell(self.x, self.y + 1)
         left = self.check_cell(self.x - 1, self.y)
+        
         if top and not top.visited:
             neighbors.append(top)
         if right and not right.visited:
@@ -54,59 +56,58 @@ class Cell:
             neighbors.append(bottom)
         if left and not left.visited:
             neighbors.append(left)
-            
+        
+        return choice(neighbors) if neighbors else None
+    
 def remove_walls(current, next):
     dx = current.x - next.x
+    
     if dx == 1:
         current.walls['left'] = False
         next.walls['right'] = False
     elif dx == -1:
         current.walls['right'] = False
         next.walls['left'] = False
+    
     dy = current.y - next.y
+    
     if dy == 1:
         current.walls['top'] = False
         next.walls['bottom'] = False
     elif dy == -1:
         current.walls['bottom'] = False
-        next.walls['top'] = False
-        
-            
-grid_cells = [Cell(col,row) for row in range(rows) for col in range(cols)]
+        next.walls['top'] = False 
+
+grid_cells = [Cell(col, row) for row in range(rows) for col in range(cols)]
 current_cell = grid_cells[0]
 stack = []
+colors, color = [], 40
 
-
-    
-
-running = True
-while running:
-    sc.fill (pygame.Color(255,255,255))
+while True:
+    sc.fill(pygame.Color('#a6d5e2'))
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-
+            pygame.quit()
+            exit()
+    
+    [cell.draw() for cell in grid_cells]
+    current_cell.visited = True
+    current_cell.draw_current_cell()
+    
+    [pygame.draw.rect(sc, colors[i], (cell.x * TILE + 2, cell.y * TILE + 2, TILE - 4, TILE - 4), border_radius=8) for i, cell in enumerate(stack)] 
+    
     next_cell = current_cell.check_neighbors()
+    
     if next_cell:
         next_cell.visited = True
-        dx = current_cell.x - next_cell.x
-        dy = current_cell.y - next_cell.y
-
-        if dx == 1:
-            current_cell.walls['left'] = False
-            next_cell.walls['right'] = False
-        elif dx == -1:
-            current_cell.walls['right'] = False
-            next_cell.walls['left'] = False
-        if dy == 1:
-            current_cell.walls['top'] = False
-            next_cell.walls['bottom'] = False
-        elif dy == -1:
-            current_cell.walls['bottom'] = False
-            next_cell.walls['top'] = False
-        current_cell = next_cell
+        stack.append(current_cell)
+        colors.append((min(color, 255), 0, 103))
+        color += 1
+        remove_walls(current_cell, next_cell)
+        current_cell = next_cell 
+    elif stack: 
+        current_cell = stack.pop()
         
-    
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(30)
