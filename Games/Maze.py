4b85c3a46a51dd9dca53c311fbe1e4388d7d38ad
NanoMars <174164929+NanoMars@users.noreply.github.com>
2024-08-14 14:35:52 +1000

    final commit for this session

--- Games/Maze.py
+++ Games/Maze.py
@@ -34,7 +34,43 @@ class Cell:
             
     def draw_current_cell(self):
         x, y = self.x * TILE, self.y * TILE
-        pygame.draw.rect(sc, pygame.Color('f70067'), (x + 2, y + 2, TILE - 2, TILE - 2))   
+        pygame.draw.rect(sc, pygame.Color('f70067'), (x + 2, y + 2, TILE - 2, TILE - 2))
+    def check_cell(self, x, y):
+        find_index = lambda x, y: x + y * cols
+        if x < 0 or x > cols - 1 or y < 0 or y > rows - 1:
+            return False
+        return grid_cells[find_index(x, y)]
+    def check_neighbors(self):
+        neighbors = []
+        top = self.check_cell(self.x, self.y - 1)
+        right = self.check_cell(self.x + 1, self.y)
+        bottom = self.check_cell(self.x, self.y + 1)
+        left = self.check_cell(self.x - 1, self.y)
+        if top and not top.visited:
+            neighbors.append(top)
+        if right and not right.visited:
+            neighbors.append(right)
+        if bottom and not bottom.visited:
+            neighbors.append(bottom)
+        if left and not left.visited:
+            neighbors.append(left)
+            
+def remove_walls(current, next):
+    dx = current.x - next.x
+    if dx == 1:
+        current.walls['left'] = False
+        next.walls['right'] = False
+    elif dx == -1:
+        current.walls['right'] = False
+        next.walls['left'] = False
+    dy = current.y - next.y
+    if dy == 1:
+        current.walls['top'] = False
+        next.walls['bottom'] = False
+    elif dy == -1:
+        current.walls['bottom'] = False
+        next.walls['top'] = False
+        
             
 grid_cells = [Cell(col,row) for row in range(rows) for col in range(cols)]
 current_cell = grid_cells[0]
@@ -51,6 +87,26 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    next_cell = current_cell.check_neighbors()
+    if next_cell:
+        next_cell.visited = True
+        dx = current_cell.x - next_cell.x
+        dy = current_cell.y - next_cell.y
 
+        if dx == 1:
+            current_cell.walls['left'] = False
+            next_cell.walls['right'] = False
+        elif dx == -1:
+            current_cell.walls['right'] = False
+            next_cell.walls['left'] = False
+        if dy == 1:
+            current_cell.walls['top'] = False
+            next_cell.walls['bottom'] = False
+        elif dy == -1:
+            current_cell.walls['bottom'] = False
+            next_cell.walls['top'] = False
+        current_cell = next_cell
+        
+    
     pygame.display.flip()
     clock.tick(60)
