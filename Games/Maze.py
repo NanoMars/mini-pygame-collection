25546b285170dd39f4ee137c5b4a4d4d9f14c25d
NanoMars <174164929+NanoMars@users.noreply.github.com>
2024-08-14 13:52:03 +1000

    oops

--- Games/Maze.py
+++ Games/Maze.py
@@ -4,3 +4,31 @@ from random import choice
 RES = WIDTH, HEIGHT = 800, 600
 TILE = 50
 cols, rows = WIDTH // TILE, HEIGHT // TILE
+
+pygame.init()
+sc = pygame.display.set_mode(RES)
+clock = pygame.time.Clock()
+
+class Cell:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+        self.walls = {
+            'top': True,
+            'right': True,
+            'bottom': True,
+            'left': True
+        }
+
+
+
+running = True
+while running:
+    sc.fill (pygame.Color(255,255,255))
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    pygame.display.flip()
+    clock.tick(60)
