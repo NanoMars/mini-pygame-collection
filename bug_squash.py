ea6c643f43cdf19723668fe3abb0b2f7c84ed4be
NanoMars <174164929+NanoMars@users.noreply.github.com>
2024-08-15 14:07:45 +1000

    getting the bug class working then I've gotta add random walk

--- bug_squash.py
+++ bug_squash.py
@@ -6,10 +6,23 @@ screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("My Pygame Template")
 
 class bug():
-    def __init__(self):
+    def __init__(self, position):
         print("Bug created")
-        
+        self.direction = 0
+        self.position = position
+    def move(self):
+        print("Bug moved")
+        self.direction += 1
+    def draw(self):
+        x = self.position[0]
+        y = self.position[1] 
+        length = 5  
+        angle = math.radians(self.direction) 
 
+        end_x = x + length * math.cos(angle)
+        end_y = y + length * math.sin(angle)
+
+        pygame.draw.line(screen, (0, 0, 0), (x, y), (end_x, end_y), 1)
 # Game loop
 running = True
 while running:
