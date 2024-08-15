6e69bbc0fbc95540c299adf5d8f517a954a51c53
NanoMars <174164929+NanoMars@users.noreply.github.com>
2024-08-15 15:45:37 +1000

    game done!

--- bug_squash.py
+++ bug_squash.py
@@ -1,12 +1,29 @@
-import pygame, noise, math, random
+import pygame, noise, math, random, subprocess
 
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Bug Squash")
 
+game_path = "main.py"
 
-
+class Blood():
+    def __init__(self, position):
+        self.position = position
+        self.radius = 5
+        self.opacity = 255
+        self.last_time = pygame.time.get_ticks()
+        self.duration = 1000
+    def draw(self):
+        Blood_surface = pygame.Surface((self.radius * 2, self.radius * 2), pygame.SRCALPHA)
+        pygame.draw.circle(Blood_surface, (255, 0, 0, int(self.opacity)), (self.radius, self.radius), self.radius)
+        screen.blit(Blood_surface, (self.position[0] - self.radius, self.position[1] - self.radius))
+    def update(self):
+        current_time = pygame.time.get_ticks()
+        time_difference = current_time - self.last_time
+        self.last_time = current_time
+        self.opacity = max(0, self.opacity - 255 * time_difference / self.duration)
+        self.radius = max(0, self.radius - 5 * time_difference / self.duration)
 class bug():
     def __init__(self, position):
         self.direction = random.randrange(0, 360)
@@ -47,8 +64,9 @@ class bug():
         end_y = y + length * math.sin(angle)
 
         pygame.draw.line(screen, (0, 0, 0), (x, y), (end_x, end_y), 1)
+    
         
-
+bloods = []
 bugs = []
 
 for i in range(random.randint(100, 500)):
@@ -63,13 +81,22 @@ while running:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = event.pos
-            bugs = [bug for bug in bugs if math.hypot(bug.position[0] - mouse_pos[0], bug.position[1] - mouse_pos[1]) >= 50]
-            print(len(bugs))
+            removed_bugs = [bug for bug in bugs if math.hypot(bug.position[0] - mouse_pos[0], bug.position[1] - mouse_pos[1]) < 50]
+            bugs = [bug for bug in bugs if bug not in removed_bugs]
+            for bug in removed_bugs:
+                bloods.append(Blood(bug.position))
+            if bugs == []:
+                subprocess.Popen(["python3", "-c", f"import game_opener; game_opener.open_game('{game_path}')"])
+                pygame.quit()
     
     for bug in bugs:
         bug.move()
         bug.draw()
 
+    for blood in bloods:
+        blood.draw()
+        blood.update()
+    
     pygame.display.flip()
 
 pygame.quit()
